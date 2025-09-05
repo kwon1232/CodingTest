7fee73821d8fd37eccdc4cc7a707afb7ce265343
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-09-05 18:45:37 +0900

    [level 3] Title: 야근 지수, Time: 27.38 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 3.81 MB, 시간: 27.84 ms
+메모리: 3.82 MB, 시간: 27.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 30일 13:36:26
+2025년 09월 05일 18:45:36
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -3,31 +3,36 @@
 #include <algorithm>
 #include <queue>
 
-
 using namespace std;
 
 long long solution(int n, vector<int> works) {
     long long answer = 0;
-
+    
     priority_queue<int> q;
     for (int i = 0; i < works.size(); i++)
     {
         q.push(works[i]);
     }
-    int temp;
-    while (!q.empty() && n != 0)
+    
+    while(n > 0 && !q.empty())
     {
+        int temp = q.top();
+        q.pop();
+        
+        if(temp > 0)
+        {
+            q.push(temp -1);
+        }
         n--;
-        temp = q.top();
-        q.pop();
-        if (temp != 1) q.push(temp - 1);
     }
-    while (!q.empty())
+    
+    while(!q.empty())
     {
-        answer += (q.top() * q.top()); 
+        long long x = q.top();
         q.pop();
+        answer += x*x;
     }
-
-
+    
+    
     return answer;
 }
