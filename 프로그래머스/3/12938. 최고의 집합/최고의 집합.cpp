724238b3b70e4ec81281744a816e95b92c20bf76
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-10-08 16:23:34 +0900

    [level 3] Title: 최고의 집합, Time: 2.34 ms, Memory: 5.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+    
+    if (s < n) 
+        return {-1};
+    
+    int q = s / n;
+    int r = s % n;
+    
+    for (int i = 0; i < n - r; i++)
+    {
+        answer.push_back(q);
+    }
+    
+    for (int i = 0; i < r; i++)
+    {
+        answer.push_back(q + 1);
+    }
+    
+    
+    return answer;
+}
