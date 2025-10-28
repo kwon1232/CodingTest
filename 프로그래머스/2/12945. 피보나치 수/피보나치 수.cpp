612e0838e6a7027b5d1ee320191d2ee08c2f4f0a
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-10-28 13:17:03 +0900

    [level 2] Title: 피보나치 수, Time: 0.56 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -1,21 +1,18 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-
-int solution(int n) 
-{
-
-    int arr[n+1];
+int solution(int n) {
+    vector<int>arr(n+1,0);
     arr[0] = 0;
     arr[1] = 1;
-    for (int i = 2; i <= n; i++)
+    arr[2] = 2;
+    
+    for(int i = 3; i <= n; i++)
     {
-        arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
     }
-
-    return arr[n];
+    
+    return arr[n-1];
 }
-
